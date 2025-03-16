fd6752c4f06d0e7da8ae35115846ac5073bc2e43
pavan7710 <bollinenipavan24111997@gmail.com>
2025-03-16 12:50:34 +0530

    basic testing

---
+++ src/app/pipe/strength/strength.pipe.spec.ts
@@ -0,0 +1,24 @@
+import { StrengthPipe } from './strength.pipe';
+
+describe('StrengthPipe', () => {
+  it('create an instance', () => {
+    const pipe = new StrengthPipe();
+    expect(pipe).toBeTruthy();
+  });
+
+  it('should display week if 5 value is passed' , () => {
+    const pipe = new StrengthPipe()
+    expect(pipe.transform(5)).toEqual('5 weak')
+  })
+
+  it('should display strong if 10 value is passed' , () => {
+    const pipe = new StrengthPipe()
+    expect(pipe.transform(10)).toEqual('10 strong')
+  }) 
+
+  it('should display the strongest if 20 value is passed' , () => {
+    const pipe = new StrengthPipe()
+    expect(pipe.transform(20)).toEqual('20 strongest')
+  })
+
+});
