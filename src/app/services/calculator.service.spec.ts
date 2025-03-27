1f99ff8ad4197105fdf8fd0be471b01418ef8139
pavan7710 <bollinenipavan24111997@gmail.com>
2025-03-27 22:49:18 +0530

    changes

--- src/app/services/calculator.service.spec.ts
+++ src/app/services/calculator.service.spec.ts
@@ -19,33 +19,97 @@ import { TestBed } from "@angular/core/testing"
 //     })
 // })
 
+// describe("CalculatorService" , () => {
+//     let mockLoggerService  : jasmine.SpyObj<LoggerService>
+//     let calculator : CalculatorService
+//     beforeEach(() => {
+//         mockLoggerService = jasmine.createSpyObj('LoggerService' , ['log']);
+//         TestBed.configureTestingModule({
+//             providers : [
+//                 CalculatorService,
+//                 {
+//                     provide: LoggerService,
+//                     useValue : mockLoggerService
+//                 }
+//             ]
+//         })
+//         calculator = TestBed.inject(CalculatorService)
+        
+//     })
+//     it("should add two numbers" , () => {
+//         let result = calculator.add(2,2)
+//         expect(result).toBe(4)
+//         expect(mockLoggerService.log).toHaveBeenCalledTimes(1)
+//     })
+//     it("should subtract two numbers" , () => {
+//         let result = calculator.subtract(2,2)
+//         expect(result).toBe(0)
+//         expect(mockLoggerService.log).toHaveBeenCalledTimes(1)
+//     })
+// })
+
+
+// describe("CalculatorService" , () => {
+//     let loggerServiceSpy  : jasmine.SpyObj<LoggerService>
+//     let calculator : CalculatorService
+//     beforeEach(() => {
+//        const mockLoggerService = jasmine.createSpyObj('LoggerService' , ['log']);
+//         TestBed.configureTestingModule({
+//             providers : [
+//                 CalculatorService,
+//                 {
+//                     provide: LoggerService,
+//                     useValue : mockLoggerService
+//                 }
+//             ]
+//         })
+//         calculator = TestBed.inject(CalculatorService)
+
+//         loggerServiceSpy = TestBed.inject(
+//             LoggerService
+//         )  as jasmine.SpyObj<LoggerService>
+//     })
+//     it("should add two numbers" , () => {
+//         let result = calculator.add(2,2)
+//         expect(result).toBe(4)
+//         expect(loggerServiceSpy.log).toHaveBeenCalledTimes(1)
+//     })
+//     it("should subtract two numbers" , () => {
+//         let result = calculator.subtract(2,2)
+//         expect(result).toBe(0)
+//         expect(loggerServiceSpy.log).toHaveBeenCalledTimes(1)
+//     })
+// })
+
+
+function setUp() {
+    const mockLoggerService = jasmine.createSpyObj('LoggerService' , ['log'])
+    TestBed.configureTestingModule({
+        providers : [
+            CalculatorService,
+            {
+                provide : LoggerService,
+                useValue : mockLoggerService
+            }
+        ]
+    });
+    const calculator = TestBed.inject(CalculatorService);
+    const loggerServiceSpy = TestBed.inject(
+        LoggerService
+    ) as jasmine.SpyObj<LoggerService>
+
+    return { calculator , loggerServiceSpy }
+}
 
 describe("CalculatorService" , () => {
-    let loggerServiceSpy  : jasmine.SpyObj<LoggerService>
-    let calculator : CalculatorService
-    beforeEach(() => {
-       const mockLoggerService = jasmine.createSpyObj('LoggerService' , ['log']);
-        TestBed.configureTestingModule({
-            providers : [
-                CalculatorService,
-                {
-                    provide: LoggerService,
-                    useValue : mockLoggerService
-                }
-            ]
-        })
-        calculator = TestBed.inject(CalculatorService)
-
-        loggerServiceSpy = TestBed.inject(
-            LoggerService
-        )  as jasmine.SpyObj<LoggerService>
-    })
     it("should add two numbers" , () => {
+        const { calculator , loggerServiceSpy } = setUp()
         let result = calculator.add(2,2)
         expect(result).toBe(4)
         expect(loggerServiceSpy.log).toHaveBeenCalledTimes(1)
     })
     it("should subtract two numbers" , () => {
+        const { calculator , loggerServiceSpy } = setUp()
         let result = calculator.subtract(2,2)
         expect(result).toBe(0)
         expect(loggerServiceSpy.log).toHaveBeenCalledTimes(1)
